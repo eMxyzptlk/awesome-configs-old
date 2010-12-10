4ea16625c81e0c8ae4ec8961a32928812b3aca24
Wael Nasreddine <wael.nasreddine@gmail.com>
2010-12-10 14:39:34 +0100

    rc.lua: rfkill is 0 not 2

--- awesome.lua
+++ awesome.lua
@@ -438,7 +438,7 @@ env = {
     reboot = "sudo /sbin/reboot",
     hibernate = "sudo /usr/sbin/pm-hibernate",
     rotate = "rotator",
-    rfkill = { block = "sudo rfkill block 2", unblock = "sudo rfkill unblock 2" },
+    rfkill = { block = "sudo rfkill block 0", unblock = "sudo rfkill unblock 0" },
 }
 
 -- Pipelets
